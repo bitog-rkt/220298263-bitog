8398cc68be2053720ebe2e1346febc15025e9225
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:29:23 +0800

    inserting the php code of reverse string: Final Commit

--- Act 12-Reverse a string.php
+++ Act 12-Reverse a string.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 12-Reverse a string</title>
 </head>
 <body>
+
+    <?php
+    $input = "chrysanthemum";
+    $reversed = "";
+
+    for ($i = strlen($input) - 1; $i >= 0; $i--) {
+        $reversed .= $input[$i];
+    }
+
+    echo "Input: \"$input\"\n";
+    echo "Output: \"$reversed\"\n";
+    ?>
     
 </body>
 </html>
