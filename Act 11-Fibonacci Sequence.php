4c49b1a7ee80691f6afb9b16ff48362bebcc6673
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:26:58 +0800

    inserting of php code of fibonacci sequence: Final Commit

--- Act 11-Fibonacci Sequence.php
+++ Act 11-Fibonacci Sequence.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 11-Fibonacci Sequence</title>
 </head>
 <body>
     
+    <?php
+    $num1 = 0;
+    $num2 = 1;
+    $count = 0;
+
+    echo "$num1 $num2 ";
+
+    while ($count < 8) {
+        $next = $num1 + $num2;
+        echo "$next ";
+    
+        $num1 = $num2;
+        $num2 = $next;
+    
+        $count++;
+    }
+    ?>
+
 </body>
 </html>
