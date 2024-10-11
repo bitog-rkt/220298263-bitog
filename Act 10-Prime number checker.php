a6bc9e9e389c391ac0352d137fbff5f5d8c0a480
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:24:02 +0800

    inserting the php code of prime checker: Final commit

--- Act 10-Prime number checker.php
+++ Act 10-Prime number checker.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 10-Prime number checker</title>
 </head>
 <body>
+
+    <?php
+    echo "Enter a number: ";
+    $number = intval(trim(fgets(STDIN)));
+    $is_prime = true;
+
+    if ($number <= 1) {
+        $is_prime = false;
+    } 
+    else {
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            if ($number % $i == 0) {
+                $is_prime = false;
+                break;
+            }
+        }
+    }
+
+    if ($is_prime) {
+        echo "$number is a prime number.\n";
+    } else {
+        echo "$number is not a prime number.\n";
+    }
+    ?>
     
 </body>
 </html>
