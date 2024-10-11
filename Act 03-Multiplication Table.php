72881e996331233e50c75bad03c4b5f3322e22c6
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 11:59:49 +0800

    inserting the php code of multiplication and changing the title: Final Commit

--- Act 03-Multiplication Table.php
+++ Act 03-Multiplication Table.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 03-Multiplication Table</title>
 </head>
 <body>
+
+    <?php
+        $num = 7;
+        for($i = 1; $i <= 10; $i++){
+            $product = $num * $i;
+            echo "$num x $i = $product \n";
+        }
+    ?>
     
 </body>
 </html>
