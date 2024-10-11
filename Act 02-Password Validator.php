e2f35c7bd4bdfc6a5bf0a98c2e3dbf893b46e2f1
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 11:51:21 +0800

    inserting php code; Final Commit

--- Act 02-Password Validator.php
+++ Act 02-Password Validator.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 02-Password Validator</title>
 </head>
 <body>
+
+    <?php
+        $correct_password="password123";
+
+        do{
+            echo "Please enter the password: ";
+            $input_password = trim(fgets(STDIN));
+
+            if($input_password != $correct_password){
+                echo "Incorrect password. \n";
+            }
+        }while ($input_password != $correct_password);
+        echo "Access Granted.";
+    ?>
     
 </body>
 </html>
