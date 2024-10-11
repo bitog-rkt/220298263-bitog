8d70391fa8490b27bb01354b273f1a62c0d30588
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:03:11 +0800

    inserting php code of loop control with break and continue: Final Commit

--- Act 04-Loop Control with break and continue.php
+++ Act 04-Loop Control with break and continue.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 04-Loop Control with break and continue</title>
 </head>
 <body>
     
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i == 5) {
+            continue;
+        }
+
+        if ($i == 8) {
+            break;
+        }
+        echo "$i ";
+    }
+?>
+
 </body>
 </html>
