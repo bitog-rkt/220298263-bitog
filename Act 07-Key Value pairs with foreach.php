e46e1a9876b693f9fe6e2d56aa1b5f45aaddd507
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:13:29 +0800

    inserting php code of key value pairs: Final Commit

--- Act 07-Key Value pairs with foreach.php
+++ Act 07-Key Value pairs with foreach.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 07-Key Value pairs with foreach</title>
 </head>
 <body>
+    
+    <?php
+    $student_info = [
+        "Name" => "Rose",
+        "Age" => 22,
+        "Grade" => "D",
+        "City" => "Baguio"];
+
+    foreach ($student_info as $key => $value) {
+        echo "$key: $value \n";
+    }
+    ?>
     
 </body>
 </html>
