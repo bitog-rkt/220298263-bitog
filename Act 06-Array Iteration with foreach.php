e125ed1a92095c142412cf2777a5f7e9aabb32a5
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:10:59 +0800

    inserting php code for array iteration: Final Commit

--- Act 06-Array Iteration with foreach.php
+++ Act 06-Array Iteration with foreach.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 06-Array Iteration with foreach</title>
 </head>
 <body>
+
+    <?php
+    $movie_name = [
+        "The Shawshank Redemption",
+        "Inception",
+        "The Dark Knight",
+        "Interstellar",
+        "Parasite"];
+
+    foreach ($movie_name as $index => $movie) {
+        echo ($index + 1) . ". " . $movie . "\n";
+    }
+    ?>
+
     
 </body>
 </html>
