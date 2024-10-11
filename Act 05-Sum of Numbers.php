55aa3e86ac154f5220bf97faf87efbc19937b36f
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:05:59 +0800

    inserting php code of sum of numbers: Final Commit

--- Act 05-Sum of Numbers.php
+++ Act 05-Sum of Numbers.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 05-Sum of Numbers</title>
 </head>
 <body>
-    
+
+    <?php
+        $sum = 0;
+        $i = 1;
+
+    while ($i <= 100) {
+        $sum += $i;
+        $i++;
+    }
+
+        echo "The sum of numbers from 1 to 100 is: $sum \n";
+    ?>
+
 </body>
 </html>
