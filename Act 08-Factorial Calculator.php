6a9498921fa8364ca87358a9d855729148290aad
Kai <152266448+rskylabitog@users.noreply.github.com>
2024-10-11 12:17:03 +0800

    inserting the php code of factorial: Final commit

--- Act 08-Factorial Calculator.php
+++ Act 08-Factorial Calculator.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Act 08-Factorial Calculator</title>
 </head>
 <body>
+
+    <?php
+    $number = 5; 
+    $factorial = 1;
+
+    for ($i = $number; $i > 0; $i--) {
+        $factorial *= $i;
+    }
+
+    echo "Factorial of $number is: $factorial \n";
+    ?>
+
     
 </body>
 </html>
